93ded9baf6b89c706cdc70862539adc3d50449bf
deep0133 <deepanshuheer27@gmail.com>
2025-03-19 15:38:52 +0530

    navbar completed , i have added font-poppin in class, but need to install poppin font family for applying on font

---
+++ src/components/Logo.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import LogoIcon from "../assets/navbar/logo.png"
+import Image from 'next/image';
+const Logo = () => {
+  return (
+    <div className="flex items-center w-[298px]">
+      <Image src={LogoIcon} className="w-[193.85px] h-[81px] text-red-600 object-contain" alt='logo' />
+      {/* <span className="text-xl font-bold">RarMotion Electric</span> */}
+    </div>
+  );
+};
+
+export default Logo;
