4f158d724513ae6aea338986732e735660140f97
Rahul Yadav <rahulyad8860@gmail.com>
2025-04-18 12:48:13 +0530

    'navbar'

--- src/components/GlobalComp/Navbar.jsx
+++ src/components/GlobalComp/Navbar.jsx
@@ -12,7 +12,7 @@ const solutions = {
     },
     {
       name: "Energy Saving Solutions",
-      href: "/EnergySavingSolutions",
+      href: "/Solutions/EnergySavingSolutions",
     },
     {
       name: "Turnkey Systems",
@@ -20,15 +20,15 @@ const solutions = {
     },
     {
       name: "Distributed Control Systems",
-      href: "/DistributedControlSystems",
+      href: "/Solutions/DistributedControlSystems",
     },
     {
       name: "Switchgear Solutions",
-      href: "/SwitchgearSolutions",
+      href: "/Solutions/SwitchgearSolutions",
     },
     {
       name: "Supervisory Control & Data Acquisition",
-      href: "/SupervisoryControlDataAcquisition",
+      href: "/Solutions/SupervisoryControlDataAcquisition",
     },
   ],
 };
@@ -52,7 +52,6 @@ const Navbar = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
 
   const handleNext = () => {
-    console.log("----handle nextLick");
     if (activeDropdown === "Solutions") {
       setCurrentIndex((prev) => (prev + 1) % solutions.items.length);
     } else if (activeDropdown === "Industries") {
@@ -64,10 +63,8 @@ const Navbar = () => {
     <nav className='bg-white shadow-lg'>
       <div className='mx-auto'>
         <div className='flex justify-between'>
-          {/* Logo */}
           <Logo />
 
-          {/* Desktop Navigation */}
           <div className='hidden lg:block destop-side-2 w-full'>
             <div className='w-full basis-full h-[51px] bg-[#939393]' />
             <div className='grid grid-cols-6 pr-8 items-center justify-center h-[74px] bg-gray-100 bg-opacity-90'>
@@ -79,16 +76,9 @@ const Navbar = () => {
                 <a href='/About' className='text-gray-700 hover:text-gray-900'>
                   About
                 </a>
-                <a
-                  href='/products'
-                  className='text-gray-700 hover:text-gray-900'
-                >
-                  Products
-                </a>
 
                 {/* Solutions Dropdown */}
                 <div
-                  // className='relative'
                   onMouseEnter={() => setActiveDropdown("Solutions")}
                   onMouseLeave={() => setActiveDropdown(null)}
                 >
@@ -96,10 +86,10 @@ const Navbar = () => {
                     Solutions
                   </button>
                   {activeDropdown === "Solutions" && (
-                    <div className='absolute z-50 inset-x-0  w-full pt-8 bg-transparent'>
+                    <div className='absolute z-50 inset-x-0 w-full pt-8 bg-transparent'>
                       <div className='shadow-xl flex bg-white '>
                         <div className='w-1/3 border-Color bg-[#ec3436]/10 p-4 rounded-l-lg'>
-                          <h3 className=' pt-6 w-[308px] mx-auto flex items-center justify-between text-center text-[#333333] text-3xl font-normal font-poppins'>
+                          <h3 className='pt-6 w-[308px] mx-auto flex items-center justify-between text-center text-[#333333] text-3xl font-normal font-poppins'>
                             Solutions
                             <ChevronRight
                               className='cursor-pointer hover:text-pink-600'
@@ -109,17 +99,17 @@ const Navbar = () => {
                         </div>
                         <div className='w-full p-12 grid grid-cols-2 gap-4'>
                           {solutions.items.map((item, index) => (
-                            <div
+                            <a
                               key={item.href}
-                              className={`justify-center text-[#333333] text-[29px] font-normal font-poppins transition-opacity duration-300 ${
+                              href={item.href}
+                              className={`block justify-center text-[#333333] text-[29px] font-normal font-poppins transition-opacity duration-300 ${
                                 index === currentIndex
                                   ? "opacity-100"
                                   : "opacity-50"
                               }`}
                             >
                               {item.name}
-                              
-                            </div>
+                            </a>
                           ))}
                         </div>
                       </div>
@@ -129,7 +119,6 @@ const Navbar = () => {
 
                 {/* Industries Dropdown */}
                 <div
-                  // className='relative'
                   onMouseEnter={() => setActiveDropdown("Industries")}
                   onMouseLeave={() => setActiveDropdown(null)}
                 >
@@ -137,10 +126,10 @@ const Navbar = () => {
                     Industries
                   </button>
                   {activeDropdown === "Industries" && (
-                    <div className='absolute z-50 inset-x-0  w-full pt-8 bg-transparent'>
+                    <div className='absolute z-50 inset-x-0 w-full pt-8 bg-transparent'>
                       <div className='shadow-xl flex bg-white '>
                         <div className='w-1/3 border-Color bg-[#ec3436]/10 p-4 rounded-l-lg'>
-                          <h3 className=' pt-6 w-[308px] mx-auto flex items-center justify-between text-center text-[#333333] text-3xl font-normal font-poppins'>
+                          <h3 className='pt-6 w-[308px] mx-auto flex items-center justify-between text-center text-[#333333] text-3xl font-normal font-poppins'>
                             Industries
                             <ChevronRight
                               className='cursor-pointer hover:text-pink-600'
@@ -150,16 +139,17 @@ const Navbar = () => {
                         </div>
                         <div className='w-full p-12 grid grid-cols-2 gap-4'>
                           {industries.items.map((item, index) => (
-                            <div
+                            <a
                               key={item}
-                              className={`justify-center text-[#333333] text-[29px] font-normal font-poppins transition-opacity duration-300 ${
+                              href='#'
+                              className={`block justify-center text-[#333333] text-[29px] font-normal font-poppins transition-opacity duration-300 ${
                                 index === currentIndex
                                   ? "opacity-100"
                                   : "opacity-50"
                               }`}
                             >
                               {item}
-                            </div>
+                            </a>
                           ))}
                         </div>
                       </div>
@@ -178,31 +168,27 @@ const Navbar = () => {
                 </a>
               </div>
 
-              {/* Right Icons */}
-              <div className='hidden col-span-2  md:flex items-center justify-end space-x-4'>
+              <div className='hidden col-span-2 md:flex items-center justify-end space-x-4'>
                 <Search className='w-5 h-5 text-red-600 stroke-1 cursor-pointer' />
                 <Globe className='w-5 h-5 text-red-600 stroke-1 cursor-pointer' />
                 <Mail className='w-5 h-5 text-red-600 stroke-1 cursor-pointer' />
               </div>
             </div>
           </div>
-          {/* Mobile menu button */}
+
+          {/* Mobile Menu Toggle */}
           <div className='lg:hidden pr-8 items-center flex justify-center'>
             <button
               onClick={() => setIsOpen(!isOpen)}
               className='text-gray-600 hover:text-gray-900'
             >
-              {isOpen ? (
-                <X className='h-6 w-6' />
-              ) : (
-                <Menu className='h-6 w-6' />
-              )}
+              {isOpen ? <X className='h-6 w-6' /> : <Menu className='h-6 w-6' />}
             </button>
           </div>
         </div>
       </div>
 
-      {/* Mobile menu */}
+      {/* Mobile Menu */}
       {isOpen && (
         <div className='lg:hidden bg-white'>
           <div className='px-2 pt-2 pb-3 space-y-1'>
@@ -219,11 +205,11 @@ const Navbar = () => {
             {/* Mobile Solutions Dropdown */}
             <div className='relative'>
               <button
-                onClick={() => {
+                onClick={() =>
                   setActiveDropdown(
                     activeDropdown === "Solutions" ? null : "Solutions"
-                  );
-                }}
+                  )
+                }
                 className='w-full text-left px-3 py-2 text-gray-700 flex justify-between items-center'
               >
                 Solutions
@@ -280,16 +266,10 @@ const Navbar = () => {
               )}
             </div>
 
-            <a
-              href='/blog'
-              className='block px-3 flex-shrink-0 py-2 text-gray-700'
-            >
+            <a href='/blog' className='block px-3 py-2 text-gray-700'>
               Blog
             </a>
-            <a
-              href='/ContactUs'
-              className='block shrink-0 px-3 py-2 text-gray-700'
-            >
+            <a href='/ContactUs' className='block px-3 py-2 text-gray-700'>
               Contact Us
             </a>
           </div>
