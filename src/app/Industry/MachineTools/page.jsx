ac6cb6098421df60ae0193fee2c964411fe65baa
jatinrajwal15 <rajwaljatin15@gmail.com>
2025-04-21 16:10:45 +0530

    Add new components and images for Machine Tools and Packaging pages

--- src/app/Industry/MachineTools/page.jsx
+++ src/app/Industry/MachineTools/page.jsx
@@ -1,9 +1,57 @@
 import React from 'react'
+import NewsStories from '../../../components/HomePageComp/NewsStories'
+
+import Herobox from '../../../components/MachineToolsComp/Herobox'
 
 const page = () => {
   return (
-    <div>page</div>
+    <>
+    <div className='mb-42'><Herobox /></div>
+      
+      <NewsStories newsData={newsData}/>
+    </>
   )
 }
 
 export default page
+
+const newsData = [
+  {
+    id: 1,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    image: "/images/NewsStories/Operator.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    image: "/images/NewsStories/Operator.png",
+    date: "March 2, 2025",
+    title: "New Tech Launch",
+    description: "The latest tech innovation is here. Discover what’s new!",
+  },
+  {
+    id: 5,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 3, 2025",
+    title: "AI Breakthrough",
+    description: "Artificial Intelligence is evolving faster than ever before.",
+  },
+];
--- src/app/Industry/Packaging/page.jsx
+++ src/app/Industry/Packaging/page.jsx
@@ -1,9 +1,54 @@
 import React from 'react'
+import Parallaxbox from '../../../components/Packagingcomp/Parallaxbox.jsx'
+import NewsStories from "../../../components/HomePageComp/NewsStories.jsx"
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Parallaxbox/>
+      <NewsStories newsData={newsData}/>
+    </div>
   )
 }
 
 export default page
+const newsData = [
+  {
+    id: 1,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 2,
+    image: "/images/NewsStories/Operator.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 3,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 1, 2025",
+    title: "Web Solution Release",
+    description:
+      "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+  },
+  {
+    id: 4,
+    image: "/images/NewsStories/Operator.png",
+    date: "March 2, 2025",
+    title: "New Tech Launch",
+    description: "The latest tech innovation is here. Discover what’s new!",
+  },
+  {
+    id: 5,
+    image: "/images/NewsStories/Plug.png",
+    date: "March 3, 2025",
+    title: "AI Breakthrough",
+    description: "Artificial Intelligence is evolving faster than ever before.",
+  },
+];
