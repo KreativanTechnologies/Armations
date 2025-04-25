2516ea4f81bd62454fef30bbbfda6f63d8c5fec5
lakshay <lakshay8219@gmail.com>
2025-04-25 19:07:27 +0530

    updated code

--- src/app/Industry/Pharma/page.jsx
+++ src/app/Industry/Pharma/page.jsx
@@ -1,40 +1,10 @@
-import React from 'react'
+import React from "react";
 
-import Pharmasection from "../../../components/Pharma/pharmaSection.jsx"
-import  Section from "../../../components/Textile/section.jsx"
-import NewsStories from "../../../components/HomePageComp/NewsStories.jsx"
-import Auto from "../../../components/Textile/auto.jsx"
+import Pharmasection from "../../../components/Pharma/pharmaSection.jsx";
+import Section from "../../../components/Textile/section.jsx";
+import NewsStories from "../../../components/HomePageComp/NewsStories.jsx";
+import Auto from "../../../components/Textile/auto.jsx";
 
-
-export const page = () => {
-  return (
-    <>
-      <div className="relative flex items-center py-4 bg-white justify-center mt-20 mb-20">
-        <div className="w-0.5 h-10 bg-white mr-4"></div>
-        <div className="text-center mb-8">
-        <h2 className="text-3xl md:text-4xl font-semibold text-black">
-          Pharmaceutical Industry <span className="text-red-500">Solutions</span>
-        </h2>
-        <div className="h-1 w-40 bg-red-500 mx-auto mt-2 rounded-full" />
-      </div>
-      </div>
-      <div className="relative w-full h-screen overflow-hidden mt-12">
-            {/* Background Image */}
-            <img
-                src="/images/about/Pharma/Rectangle 115.png" 
-                alt="Plastic Industry Background"
-                className="absolute inset-0 w-[calc(90%-80px)] object-cover opacity-90 mx-auto"
-                width={200}
-                height={200}
-            />
-      </div>
-      <Pharmasection />
-      <Auto/>
-      <Section />
-      <NewsStories newsData={newsData} />
-    </>
-  );
-}
 const newsData = [
   {
     id: 1,
@@ -76,4 +46,33 @@ const newsData = [
   },
 ];
 
-export default page
+export default function Page() {
+  return (
+    <>
+      <div className="relative flex items-center py-4 bg-white justify-center mt-20 mb-20">
+        <div className="w-0.5 h-10 bg-white mr-4"></div>
+        <div className="text-center mb-8">
+          <h2 className="text-3xl md:text-4xl font-semibold text-black">
+            Pharmaceutical Industry{" "}
+            <span className="text-red-500">Solutions</span>
+          </h2>
+          <div className="h-1 w-40 bg-red-500 mx-auto mt-2 rounded-full" />
+        </div>
+      </div>
+      <div className="relative w-full h-screen overflow-hidden mt-12">
+        {/* Background Image */}
+        <img
+          src="/images/about/Pharma/Rectangle 115.png"
+          alt="Plastic Industry Background"
+          className="absolute inset-0 w-[calc(90%-80px)] object-cover opacity-90 mx-auto"
+          width={200}
+          height={200}
+        />
+      </div>
+      <Pharmasection />
+      <Auto />
+      <Section />
+      <NewsStories newsData={newsData} />
+    </>
+  );
+}
