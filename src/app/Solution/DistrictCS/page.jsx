379849ee73b8a4634f960faabb8879cfab959e25
lakshay <lakshay8219@gmail.com>
2025-05-26 09:48:28 +0530

    code merging

--- src/app/Solution/DistrictCS/page.jsx
+++ src/app/Solution/DistrictCS/page.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import DCSContent from '../../../components/DCSComp/DCSContent.jsx'
+import DCSContent from '../../../components/DCScomp/DCSContent'
 // import DCSInfo from '../../../components/DCSComp/DCSInfo.jsx'
 // import DCSLayout from '../../../components/DCSComp/DCSLayout.jsx'
-import HeroSection from '../../../components/DCSComp/HeroSection.jsx'
+import HeroSection from '../../../components/DCScomp/HeroSection'
 const page = () => {
   return (
     <div>
