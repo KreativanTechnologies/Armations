69f03cba1af5115f62d138d66c73e626d5f93d9b
lakshay <lakshay8219@gmail.com>
2025-05-26 11:10:41 +0530

    code merging

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,3 +1,4 @@
+'use client'
 import React from "react";
 import AtArmation from "../../components/HomePageComp/AtArmation.jsx";
 import HeroSection from "../../components/HomePageComp/HeroSection.jsx";
