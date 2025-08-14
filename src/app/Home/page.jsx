d228a47701c2e08449440af34acf99e1735518e9
Rahul Yadav <rahulyad8860@gmail.com>
2025-08-14 11:36:38 +0530

    'error'

--- src/app/Home/page.jsx
+++ src/app/Home/page.jsx
@@ -1,46 +1,4 @@
-// 'use client'
-// import React from "react";
-// import AtArmation from "../../components/HomePageComp/AtArmation.jsx";
-// import HeroSection from "../../components/HomePageComp/HeroSection.jsx";
-// import NewsStories from "../../components/HomePageComp/NewsStories.jsx";
-// import OurAchievements from "../../components/HomePageComp/OurAchievements.jsx";
-// import OurMission from "../../components/HomePageComp/OurMission.jsx";
-// import Parallax from "../../components/HomePageComp/Parallax.jsx";
-// import { Zap, Sun } from "lucide-react";
-// const heroSectionData = {
-//   img_Src: "/images/home/bg.png",
-//   title: "Aarmation Electric",
-//   desc: "Our end-to-end smart solutions empower smart industries, smart living, and smart enterprises and deliver connected experiences by connecting assets, operation/logistics, and services.",
-//   btn_Text: "Explore more",
-// };
 
-// const OurVisionData = {
-//   title: "Our Vision",
-//   desc: "Our vision is to position “AARMATION ELECTRIC” globally as a leading project company in Electrical, IoT, SCADA, Artificial Intelligence, Automation Solutions, and Instrumentation by 2030.",
-//   btn_Text: "Let's go",
-//   img_Src: "/om.png",
-// };
-// const ParallaxData = {
-//   title: "Our Mission",
-//   desc: "Our end to end smart solutions empower smart industries, smart living and smart enterprises and deliver connecting experiences by connecting assets, operation/ logistics and services.",
-//   btn_Text: "Learn More About Us",
-//   redirectTo: "/About",
-//   img_Src: "/om.png",
-//   list: [
-//     {
-//       icon: Zap,
-//       text: "Energy Saving Solution",
-//     },
-//     {
-//       icon: Sun,
-//       text: "Distributed Control System",
-//     },
-//   ],
-// };
-
-// const ParallaxData2 = {
-//   title: "Did You Know?",
-//   desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic.",
 //   btn_Text: "Learn More",
 //   redirectTo: "/About",
 //   img_Src: "/om.png",
