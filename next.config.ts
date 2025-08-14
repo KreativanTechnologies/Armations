481472cdff977a96d70c5f69c65ef1541ddc3124
Rahul Yadav <rahulyad8860@gmail.com>
2025-08-14 11:49:05 +0530

    'tsx'

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  trailingSlash: true,
-};
-
-export default nextConfig;
-
-
-
-
-// import type { NextConfig } from 'next';
+// import type { NextConfig } from "next";
 
 // const nextConfig: NextConfig = {
-//   output: 'export', // This enables static export mode
+//   trailingSlash: true,
 // };
 
 // export default nextConfig;
+
+
+
+
+import type { NextConfig } from 'next';
+
+const nextConfig: NextConfig = {
+  output: 'export', // This enables static export mode
+};
+
+export default nextConfig;
